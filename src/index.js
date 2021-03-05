3d743587cd489728c708e69e0bc8db0ca768a591
Vladislav <pisma.drgn@mail.ru>
2021-03-05 21:28:20 +0400

    feat: add functions: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == undefined || array.length === 0){
+    return 0;
+  }
+  let numMin = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < numMin) {
+      numMin = array[i];
+    }
+  }
+  return numMin;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array == undefined || array.length === 0) {
+    return 0;
+  }
+  let numMax = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > numMax) {
+      numMax = array[i];
+    }
+  }
+  return numMax;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array == undefined || array.length === 0){
+    return 0;
+  }
+  let numAvg = 0;
+  for (let i = 0; i < array.length; i++) {
+    numAvg += array[i];
+  }
+  numAvg /= array.length;
+  return numAvg;
+};
